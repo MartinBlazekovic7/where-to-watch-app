88a4f8acd03ea1b94e78c47ddfe49c542404edba
Martin Blažeković <martinblazekovic7@gmail.com>
2023-02-17 01:50:18 +0100

    #3 - add initial html and css

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faTv } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-
+  faTelevision = faTv;
 }
