1d855cf069d9895915574d125ce68f36b2d9fc7d
Martin Blažeković <martinblazekovic7@gmail.com>
2023-02-18 11:20:28 +0100

    #5 - finish html of search and options

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -28,6 +28,16 @@ import { Item } from 'src/app/models/item.model';
         animate('0.5s ease-in', style({ height: 0, opacity: 0 })),
       ]),
     ]),
+    trigger('inOutAnimationBigger', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('0.5s ease-out', style({ height: 600, opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ height: 300, opacity: 1 }),
+        animate('0.5s ease-in', style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
   ],
 })
 export class MainComponent implements OnInit {
